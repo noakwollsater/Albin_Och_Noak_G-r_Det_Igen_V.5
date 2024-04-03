a26ace174702f2a25c23fd6d7507c47a7d311a79
Albin Hall <albin-hall1@hotmail.com>
2024-04-03 11:09:42 +0200

    hej

---
+++ Webb_Shop_Weapons/Models/Ammo.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Webb_Shop_Weapons.Models
+{
+    public class Ammo
+    {
+        public int AmmoID { get; set; }
+        public string Name { get; set; }
+        public ICollection<Weapon> Weapon { get; set; }
+    }
+}
