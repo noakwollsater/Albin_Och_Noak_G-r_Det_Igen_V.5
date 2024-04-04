107dc6160301526ac118b384c702c5690bbd2462
noakwollsater <142594779+noakwollsater@users.noreply.github.com>
2024-04-04 17:12:25 +0200

    Vet inte ens

--- Webb_Shop_Weapons/Pages/Index.cshtml.cs
+++ Webb_Shop_Weapons/Pages/Index.cshtml.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System;
 using Webb_Shop_Weapons.Data;
 using Webb_Shop_Weapons.Models;
 
@@ -8,17 +10,34 @@ namespace Webb_Shop_Weapons.Pages
     public class IndexModel : PageModel
     {
         private readonly AppDbContext database;
-
+        public List<Weapon> weapons { get; set; } = new List<Weapon>();
         public IndexModel(AppDbContext database)
         {
             this.database = database;
         }
 
-        public List<Weapon> weapons { get; set; }
+        public bool prevPage { get; set; }
+        public bool nextPage { get; set; }
+        public int currentPage { get; set; }
 
-        public void OnGet()
+        public void OnGet(int page = 1)
         {
-            weapons = database.Weapons.ToList();
+            int weaponCount = 10;
+            int totalWeapons = database.Weapons.Count();
+            int totalPages = (int)Math.Ceiling((double)totalWeapons / weaponCount);
+            currentPage = page;
+            int startIndex = (page - 1) * weaponCount;
+            weapons = database.Weapons.Skip(startIndex).Take(weaponCount).ToList();
+            prevPage = page > 1;
+            nextPage = page < totalPages;
+        }
+        public IActionResult OnGetPrevPage(int page)
+        {
+            return RedirectToPage("/Index.cshtml", new { currentPage = page - 1 });
+        }
+        public IActionResult OnGetNextPage(int page)
+        {
+            return RedirectToPage("/Index", new { page = page + 1 });
         }
     }
 }
