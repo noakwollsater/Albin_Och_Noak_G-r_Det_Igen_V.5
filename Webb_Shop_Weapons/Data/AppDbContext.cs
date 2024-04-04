674d751c4093ff78d7ea714a1cea55561aaa5cf1
Albin Hall <albin-hall1@hotmail.com>
2024-04-04 10:48:21 +0200

    dbcontext override

--- Webb_Shop_Weapons/Data/AppDbContext.cs
+++ Webb_Shop_Weapons/Data/AppDbContext.cs
@@ -9,11 +9,27 @@ namespace Webb_Shop_Weapons.Data
         public DbSet<Ammo> Ammos { get; set; }
         public DbSet<Weapon> Weapons { get; set; }
         public DbSet<Account> Accounts { get; set; }
-
+        public DbSet<CartItem> CartItems { get; set; }
+        public DbSet<WeaponCartItem> WeaponCartItems { get; set; }
+        public DbSet<AmmoCartItem> AmmoCartItems { get; set; }
+        public DbSet<ShoppingCart> ShoppingCarts { get; set; }
 
         public AppDbContext(DbContextOptions<AppDbContext> options)
             : base(options)
         {
+        }
+        //vet ej om det här behövs??
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<WeaponCartItem>()
+                .HasOne(wci => wci.Weapon)
+                .WithMany(w => w.WeaponCartItems)
+                .HasForeignKey(wci => wci.WeaponId);
+
+            modelBuilder.Entity<AmmoCartItem>()
+                .HasOne(aci => aci.Ammo)
+                .WithMany(a => a.AmmoCartItems)
+                .HasForeignKey(aci => aci.AmmoID);
         }
     }
 }
