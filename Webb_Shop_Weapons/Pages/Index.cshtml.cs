1b8348bef1f1f64d7fa69613f2cb8ee0070e33b4
noakwollsater <142594779+noakwollsater@users.noreply.github.com>
2024-04-08 16:18:52 +0200

    Searchbar

--- Webb_Shop_Weapons/Pages/Index.cshtml.cs
+++ Webb_Shop_Weapons/Pages/Index.cshtml.cs
@@ -21,15 +21,26 @@ namespace Webb_Shop_Weapons.Pages
         public int CurrentPage { get; set; }
         public bool HasNextPage { get; set; }
         public bool HasPreviousPage { get; set; }
+        public string SearchBar { get; set; }
 
-        public IActionResult OnGet(int pageID = 0)
+
+        public IActionResult OnGet(int pageID = 0, string searchBar = null)
         {
             CurrentPage = pageID;
+            SearchBar = searchBar;
 
-            var totalCount = database.Weapons.Count();
+            IQueryable<Weapon> weaponsearch = database.Weapons;
+
+            if (!string.IsNullOrEmpty(SearchBar))
+            {
+                weaponsearch = weaponsearch.Where(weapon => weapon.Name.Contains(SearchBar));
+            }
+
+            var totalCount = weaponsearch.Count();
             var totalPages = (int)Math.Floor(totalCount / (double)PageSize);
 
-            Weapons = database.Weapons
+
+            Weapons = weaponsearch
                 .Skip((CurrentPage) * PageSize)
                 .Take(PageSize)
                 .ToList();
