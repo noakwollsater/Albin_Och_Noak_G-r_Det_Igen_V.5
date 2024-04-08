ae09f25ee6bae42344693413cfca0c98dff8e37e
noakwollsater <142594779+noakwollsater@users.noreply.github.com>
2024-04-08 12:31:56 +0200

    Paging

--- Webb_Shop_Weapons/Pages/WeaponInfo.cshtml.cs
+++ Webb_Shop_Weapons/Pages/WeaponInfo.cshtml.cs
@@ -1,12 +1,38 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Webb_Shop_Weapons.Data;
+using Webb_Shop_Weapons.Models;
 
 namespace Webb_Shop_Weapons.Pages
 {
     public class WeaponInfoModel : PageModel
     {
-        public void OnGet()
+        private readonly AppDbContext database;
+
+        public Weapon Weapon { get; set; }
+        public Category Category { get; set; }
+        public Ammo Ammo { get; set; }
+
+        public WeaponInfoModel(AppDbContext database)
         {
+            this.database = database;
+        }
+
+        public IActionResult OnGet(int id)
+        {
+            Weapon = database.Weapons.Find(id);
+
+            Category = database.Categories.Find(Weapon.CategoryId);
+
+            Ammo = database.Ammos.Find(Weapon.AmmoId);
+
+            if (Weapon == null)
+            {
+                return NotFound();
+            }
+
+            return Page();
         }
     }
+
 }
