667494314addc66c14268e211dc339e7d9defcb4
Albin Hall <albin-hall1@hotmail.com>
2024-04-11 17:18:55 +0200

    add functionallity to the item shoppincart button

--- Webb_Shop_Weapons/Pages/Index.cshtml.cs
+++ Webb_Shop_Weapons/Pages/Index.cshtml.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using System;
+using System.Security.Cryptography.X509Certificates;
 using Webb_Shop_Weapons.Data;
 using Webb_Shop_Weapons.Models;
 
@@ -10,11 +11,15 @@ namespace Webb_Shop_Weapons.Pages
     public class IndexModel : PageModel
     {
         private readonly AppDbContext database;
+        private readonly AccessControl accessControl;
         public List<Weapon> weapons { get; set; } = new List<Weapon>();
         private const int PageSize = 10;
-        public IndexModel(AppDbContext database)
+        public ShoppingCart shoppingCart { get; set; }
+
+        public IndexModel(AppDbContext database, AccessControl accessControl)
         {
             this.database = database;
+            this.accessControl = accessControl;
         }
 
         public IList<Weapon> Weapons { get; set; }
@@ -36,6 +41,8 @@ namespace Webb_Shop_Weapons.Pages
                 weaponsearch = weaponsearch.Where(weapon => weapon.Name.Contains(SearchBar));
             }
 
+            
+
             var totalCount = weaponsearch.Count();
             var totalPages = (int)Math.Floor(totalCount / (double)PageSize);
 
@@ -50,6 +57,66 @@ namespace Webb_Shop_Weapons.Pages
 
             return Page();
         }
+
+        public IActionResult OnPostAddToCart(int productId)
+        {
+            var currentUser = database.Accounts.FirstOrDefault(a => a.ID == accessControl.LoggedInAccountID);
+            if (currentUser == null)
+            {
+                return RedirectToPage("index");
+            }
+
+            shoppingCart = LoadUserShoppingCart(currentUser.ID);
+
+
+            if (shoppingCart == null)
+            {
+                shoppingCart = new ShoppingCart { AccountId = currentUser.ID };
+                database.ShoppingCarts.Add(shoppingCart);
+            }
+
+
+            var item = shoppingCart.Items.FirstOrDefault(i => i.Product.ProductId == productId);
+
+            if (item != null)
+            {
+
+                item.Quantity += 1;
+            }
+            else
+            {
+
+                var newItem = new CartItem
+                {
+                    ProductId = productId,
+                    Quantity = 1,
+                    ShoppingCartId = shoppingCart.ShoppingCartId
+                };
+                shoppingCart.Items.Add(newItem);
+            }
+
+            database.SaveChanges();
+            return RedirectToPage();
+        }
+
+        public ShoppingCart LoadUserShoppingCart(int userId)
+        {
+            var shoppingCart = database.ShoppingCarts
+                           .FirstOrDefault(cart => cart.AccountId == userId);
+
+            if (shoppingCart != null)
+            {
+                shoppingCart = database.ShoppingCarts
+                           .Include(cart => cart.Items)
+                           .ThenInclude(item => item.Product)
+                           .FirstOrDefault(cart => cart.AccountId == userId);
+            }
+
+            return shoppingCart;
+        }
+
+
+
 
     }
 }
