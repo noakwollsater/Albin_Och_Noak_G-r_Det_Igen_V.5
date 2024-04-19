bbc343886de7e751c56dc95927e00b823619cf0e
Albin Hall <albin-hall1@hotmail.com>
2024-04-19 12:09:27 +0200

    funkar

--- Webb_Shop_Weapons/Program.cs
+++ Webb_Shop_Weapons/Program.cs
@@ -107,7 +107,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI(options =>
     {
         options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-        options.RoutePrefix = string.Empty;
+        options.RoutePrefix = "swagger"; 
     });
     app.UseStaticFiles();
 }
